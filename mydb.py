354916d4661304c2321d80653adf0b86d8b40610
Sikiru Salau <salau.olamilekan@gmail.com>
2023-09-28 01:40:46 +0100

    first commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+  host = 'localhost',
+  user = 'root',
+  passwd = 'lake2root')
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
